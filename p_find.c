1f51c142a9c025668ea7b6389539aebfdec5d281
Anna <muturiwannah83@gmail>
2023-04-27 15:18:52 -0700

    path

--- p_find.c
+++ p_find.c
@@ -1,50 +1,123 @@
 #include "alx.h"
-/**
- *findpath -  gets the complete full PATH of the commands
- *@command: is the input by the user
- *@env: is the environment to find path
- *Return: status of 1 or 0
- */
-int findpath(char **command, char **env)
-{
-	char *left = NULL, *right = NULL, *bins, *token, *envdup, *path = NULL;
-	size_t i, j, len1 = 0, len2 = 0;
 
-	for (i = 0; env[i]; i++)
+int check_file(char *full_path);
+
+/**
+ * find_program - find a program in path
+ * @data: a pointer to the program's data
+ * Return: 0 if success, errcode otherwise
+ */
+
+int find_program(data_of_program *data)
+{
+	int i = 0, ret_code = 0;
+	char **directories;
+
+	if (!data->command_name)
+		return (2);
+
+	/**if is a full_path or an executable in the same path */
+	if (data->command_name[0] == '/' || data->command_name[0] == '.')
+		return (check_file(data->command_name));
+
+	free(data->tokens[0]);
+	data->tokens[0] = str_concat(str_duplicate("/"), data->command_name);
+	if (!data->tokens[0])
+		return (2);
+
+	directories = tokenize_path(data);/* search in the PATH */
+
+	if (!directories || !directories[0])
 	{
-		envdup = _strdup(env[i]);
-		left = strtok(envdup, "=");
-		right = strtok(NULL, "=");
-		if (_strcmp(left, "PATH") == 0)
-		{
-			bins = right;
-			token = strtok(bins, ": \t");
-			for (j = 0; token; j++)
-			{
-				len1 = _strlen(token);
-				len2 = _strlen(command[0]);
-				path = malloc(sizeof(char) * (len1 + len2 + 2));
-				path[0] = '\0';
-				path = cat(path, token, command);
-				if (access(path, X_OK) == 0)
-				{
-					if (fork() == 0)
-					{
-						execve(path, command, NULL);
-						free(path);
-					}
-					else
-						wait(NULL);
-					free(path);
-					free(envdup);
-					return (0);
-				}
-				token = strtok(NULL, ": \t");
-				free(path);
-			}
-		}
-		free(envdup);
+		errno = 127;
+		return (127);
 	}
-	return (1);
+	for (i = 0; directories[i]; i++)
+	{/* appends the function_name to path */
+		directories[i] = str_concat(directories[i], data->tokens[0]);
+		ret_code = check_file(directories[i]);
+		if (ret_code == 0 || ret_code == 126)
+		{/* the file was found, is not a directory and has execute permisions*/
+			errno = 0;
+			free(data->tokens[0]);
+			data->tokens[0] = str_duplicate(directories[i]);
+			free_array_of_pointers(directories);
+			return (ret_code);
+		}
+	}
+	free(data->tokens[0]);
+	data->tokens[0] = NULL;
+	free_array_of_pointers(directories);
+	return (ret_code);
 }
 
+/**
+ * tokenize_path - tokenize the path in directories
+ * @data: a pointer to the program's data
+ * Return: array of path directories
+ */
+
+char **tokenize_path(data_of_program *data)
+{
+	int i = 0;
+	int counter_directories = 2;
+	char **tokens = NULL;
+	char *PATH;
+
+	/* get the PATH value*/
+	PATH = env_get_key("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{/*path not found*/
+		return (NULL);
+	}
+
+	PATH = str_duplicate(PATH);
+
+	/* find the number of directories in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			counter_directories++;
+	}
+
+	/* reserve space for the array of pointers */
+	tokens = malloc(sizeof(char *) * counter_directories);
+
+	/*tokenize and duplicate each token of path*/
+	i = 0;
+	tokens[i] = str_duplicate(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = str_duplicate(_strtok(NULL, ":"));
+	}
+
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+
+}
+
+/**
+ * check_file - checks if exists a file, if it is not a dairectory and
+ * if it has excecution permisions for permisions.
+ * @full_path: pointer to the full file name
+ * Return: 0 on success, or error code if it exists.
+ */
+
+int check_file(char *full_path)
+{
+	struct stat sb;
+
+	if (stat(full_path, &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) ||  access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	/*if not exist the file*/
+	errno = 127;
+	return (127);
+}
