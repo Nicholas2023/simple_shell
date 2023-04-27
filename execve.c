b222ec0ea4d51131ce58fb611e3b837fbddadbe2
Anna <muturiwannah83@gmail>
2023-04-27 15:26:07 -0700

    exce

--- execve.c
+++ execve.c
@@ -1,33 +1,47 @@
 #include "alx.h"
-
 /**
- *printenvi - is a built-in that will print the enviroment
- *@env: is the enviroment
- *Return: void
+ * execute - execute a command with its entire path variables.
+ * @data: a pointer to the program's data
+ * Return: If sucess returns zero, otherwise, return -1.
  */
-void printenvi(char **env)
+int execute(data_of_program *data)
 {
-	size_t i = 0;
-	size_t len;
+	int retval = 0, status;
+	pid_t pidd;
 
-	while (env[i])
-	{
-		len = _strlen(env[i]);
-		write(STDOUT_FILENO, env[i], len);
-		_putchar('\n');
-		i++;
+	/* check for program in built ins */
+	retval = builtins_list(data);
+	if (retval != -1)/* if program was found in built ins */
+		return (retval);
+
+	/* check for program file system */
+	retval = find_program(data);
+	if (retval)
+	{/* if program not found */
+		return (retval);
 	}
-}
-/**
- *_env - is a helper function that gets the env
- *@str: prints the env
- *@env: gets the env
- *Return: void
- */
-void _env(char *str, char **env)
-{
-	if (_strcmp(str, "env") == 0)
-	{
-		printenvi(env);
+	else
+	{/* if program was found */
+		pidd = fork(); /* create a child process */
+		if (pidd == -1)
+		{ /* if the fork call failed */
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{/* I am the child process, I execute the program*/
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1) /* if error when execve*/
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else
+		{/* I am the father, I wait and check the exit status of the child */
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
+		}
 	}
+	return (0);
 }
