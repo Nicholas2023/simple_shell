493d86cd805d8982e93c75ef7904425907a10a58
Anna <muturiwannah83@gmail>
2023-04-27 15:02:26 -0700

    file update

--- func_library.c
+++ func_library.c
@@ -1,99 +1,154 @@
 #include "alx.h"
 
 /**
- * _strcmp - Compare two strings
- * @nick: A pointer to the first string
- * @anna: A pointer to the second string
- *
- * Return: 0 if equal and positive or negative
- * number if unmatched characters are found
+ * str_length - returns the length of a string.
+ * @string: pointer to string.
+ * Return: length of string.
  */
-
-int _strcmp(char *nick, char *anna)
+int str_length(char *string)
 {
-	while ((*nick && *anna) && (*nick == *anna))
+	int length = 0;
+
+	if (string == NULL)
+		return (0);
+
+	while (string[length++] != '\0')
 	{
-		nick++;
-		anna++;
 	}
-	return (*nick - *anna);
+	return (--length);
 }
 
-
-
 /**
- *_strlen - Measures string length
- * @anna: A pointer to string to be measured
- *
- * Return: Integer length of the string
+ * str_duplicate - duplicates an string
+ * @string: String to be copied
+ * Return: pointer to the array
  */
-
-int _strlen(char *anna)
+char *str_duplicate(char *string)
 {
-	int _len = 0;
+	char *result;
+	int length, i;
 
-	while (anna[_len])
-		_len++;
-	return (_len);
-}
-
-
-/**
- * _strcpy - Copies string from one variable to another
- * @nick: A pointer to the source string
- * @anna: A pointer to destination string
- *
- * Return: A pointer to destination string
- */
-
-char *_strcpy(char *nick, char *anna)
-{
-	int i = -1;
-
-	do {
-		i++;
-		nick[i] = anna[i];
-	} while (anna[i] != '\0');
-
-	return (anna);
-}
-
-
-/**
- * _strdup - Allocates space in heap memory
- * @anna: A pointer to a string to allocate memory
- *
- * Return: A pointer to allocated heap memory
- */
-
-
-char *_strdup(char *anna)
-{
-	char *ptr;
-	int len = 0;
-
-	if (anna == NULL)
+	if (string == NULL)
 		return (NULL);
 
-	while (*anna++)
-		len++;
-	ptr = malloc(sizeof(char) * (len + 1));
+	length = str_length(string) + 1;
 
-	if (!ptr)
+	result = malloc(sizeof(char) * length);
+
+	if (result == NULL)
+	{
+		errno = ENOMEM;
+		perror("Error");
 		return (NULL);
-	for (len++; len--;)
-		ptr[len] = *--anna;
-	return (ptr);
+	}
+	for (i = 0; i < length ; i++)
+	{
+		result[i] = string[i];
+	}
+
+	return (result);
 }
 
 /**
- * _putchar - Writes character c to stdout
- * @c: character to be printed
- *
- * Return: 1 if success and 0 if otherwise
+ * str_compare - Compare two strings
+ * @string1: String one, or the shorter
+ * @string2: String two, or the longer
+ * @number: number of characters to be compared, 0 if infinite
+ * Return: 1 if the strings are equals,0 if the strings are different
  */
-
-int _putchar(char c)
+int str_compare(char *string1, char *string2, int number)
 {
-	return (write(1, &c, 1));
+	int iterator;
+
+	if (string1 == NULL && string2 == NULL)
+		return (1);
+
+	if (string1 == NULL || string2 == NULL)
+		return (0);
+
+	if (number == 0) /* infinite longitud */
+	{
+		if (str_length(string1) != str_length(string2))
+			return (0);
+		for (iterator = 0; string1[iterator]; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+				return (0);
+		}
+		return (1);
+	}
+	else /* if there is a number of chars to be compared */
+	{
+		for (iterator = 0; iterator < number ; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+			return (0);
+		}
+		return (1);
+	}
+}
+
+/**
+ * str_concat - concatenates two strings.
+ * @string1: String to be concatenated
+ * @string2: String to be concatenated
+ *
+ * Return: pointer to the array
+ */
+char *str_concat(char *string1, char *string2)
+{
+	char *result;
+	int length1 = 0, length2 = 0;
+
+	if (string1 == NULL)
+		string1 = "";
+	length1 = str_length(string1);
+
+	if (string2 == NULL)
+		string2 = "";
+	length2 = str_length(string2);
+
+	result = malloc(sizeof(char) * (length1 + length2 + 1));
+	if (result == NULL)
+	{
+		errno = ENOMEM;
+		perror("Error");
+		return (NULL);
+	}
+
+	/* copy of string1 */
+	for (length1 = 0; string1[length1] != '\0'; length1++)
+		result[length1] = string1[length1];
+	free(string1);
+
+	/* copy of string2 */
+	for (length2 = 0; string2[length2] != '\0'; length2++)
+	{
+		result[length1] = string2[length2];
+		length1++;
+	}
+
+	result[length1] = '\0';
+	return (result);
+}
+
+
+/**
+ * str_reverse - reverses a string.
+ *
+ * @string: pointer to string.
+ * Return: void.
+ */
+void str_reverse(char *string)
+{
+
+	int i = 0, length = str_length(string) - 1;
+	char hold;
+
+	while (i < length)
+	{
+		hold = string[i];
+		string[i++] = string[length];
+		string[length--] = hold;
+	}
 }
