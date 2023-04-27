80523708eaff31e5b347bcf8a63a4c9be0506c4b
Anna <muturiwannah83@gmail>
2023-04-26 18:58:17 -0700

    enviroment

--- execve.c
+++ execve.c
@@ -1,43 +1,32 @@
 #include "alx.h"
-
 /**
- * _execute - function that executes in the main shell
- * @status: the status of the path, whether the file found is executable or not
- * @args: the parsed arguments
- * @ex_st: the exit status
- * @tal: tally of commands or attempted commands that were run
- * Return: void
+ *printenvi - is a built-in that will print the enviroment
+ *@env: is the enviroment
+ *Return: void
  */
-void _execute(int status, char **args, int *ex_st, int *tal)
+void printenvi(char **env)
 {
-	if (status == 2)
-	{
-		if (access(args[0], X_OK) == 0)
-		{
-			if (fork() == 0)
-				execve(args[0], args, NULL);
+	size_t i = 0;
+	size_t len;
 
-			else
-				wait(NULL);
-			*ex_st = 0;
-		}
-		else if (access(args[0], F_OK) != 0)
-		{
-			print_str("sh: ");
-			print_int(tal);
-			print_str(": ");
-			perror(args[0]);
-			*ex_st = 127;
-		}
-		else if (access(args[0], F_OK) == 0 &&
-			 access(args[0], X_OK) != 0)
-		{
-			print_str("sh: ");
-			print_int(tal);
-			print_str(": ");
-			perror(args[0]);
-			*ex_st = 126;
-		}
+	while (env[i])
+	{
+		len = _strlen(env[i]);
+		write(STDOUT_FILENO, env[i], len);
+		_putchar('\n');
+		i++;
 	}
-	free(args);
+}
+/**
+ *_env - is a helper function that gets the env
+ *@str: prints the env
+ *@env: gets the env
+ *Return: void
+ */
+void _env(char *str, char **env)
+{
+	if (_strcmp(str, "env") == 0)
+	{
+		printenvi(env);
+	}
 }
