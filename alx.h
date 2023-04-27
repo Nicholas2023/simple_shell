713d19e301d29fedbfa1f048f44a166a28a64845
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-04-27 03:47:40 +0300

    main file

--- alx.h
+++ alx.h
@@ -1,33 +1,50 @@
 #ifndef ALX_H
 #define ALX_H
 
+#define TRUE (1 == 1)
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <fcntl.h>
 #include <sys/stat.h>
-#include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int _strcmp(char *nick, char *anna);
-int _strlen(char *anna);
-int _putchar(char x);
-char *_strcpy(char *nick, char *anna);
-char *_strdup(char *anna);
-char *_strcat(char *nick, char *anna);
-void p_env(char **anna);
-char **_token(char *str);
-int _path(char **nick, char **env);
-void _getenv(char *nick, char **env);
-char *cat(char *nick, char *anna, char **ven);
-void s_exit(char *anna);
-void _execve(char **nick, char *anna);
-ssize_t _getline(char **lineptr, ssize_t *n, FILE *stream);
-char **_strtok(char *input, char *dlm);
-void *_realloc(void *ptr, unsigned int o_size, unsigned int n_size);
+/* path */
+int _path(char *first, char **input, char **env, int *ex_st);
 
+
+/* environment */
+int printenv(char **env, int *ex_st);
+char *cpy_env(char **env);
+
+
+/* built-ins & execute function */
+void builtins(char *line, char **args, char **env, int *ex_st);
+void _execute(int status, char **args, int *ex_st, int *tal);
+
+
+/* strings.c: functions related to strings */
+int _strlen(char *s);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+
+
+/* printf.c: functions related to printing strings/characters/numbers */
+int _putchar(char c);
+void print_str(char *s);
+char *pathstr(char *right, char *first);
+char **parser(char *l);
+void print_int(int *tal);
+
+
+/* helpers.c: helper functions */
+void no_nl(char *l);
+void free_grid(char **grid, int height);
+int special_char(char *buffer, ssize_t bytes, int *ex_st);
+
+#endif
 #endif /*ALX_H*/
