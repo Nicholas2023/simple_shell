c56310b4d08d35ffe8f798999699930eadf7075d
Anna <muturiwannah83@gmail>
2023-04-28 18:36:01 -0700

    file update

--- execve.c
+++ execve.c
@@ -1,41 +1,43 @@
 #include "alx.h"
+
 /**
- * execute - execute a command with its entire path variables.
- * @nick: a pointer to the program's data
- * Return: If sucess returns zero, otherwise, return -1.
+ * execute - executes a command.
+ * @nick: a pointer to struct
+ * Return: 0 if successfull.
  */
 int execute(_st *nick)
 {
 	int retval = 0, status;
 	pid_t pidd;
 
-	/* check for program in built ins */
 	retval = builtins_list(nick);
-	if (retval != -1)/* if program was found in built ins */
+	if (retval != -1)
 		return (retval);
 
-	/* check for program file system */
 	retval = find_program(nick);
 	if (retval)
-	{/* if program not found */
+	{
 		return (retval);
 	}
 	else
-	{/* if program was found */
-		pidd = fork(); /* create a child process */
+	{
+		/* create a child process */
+		pidd = fork();
 		if (pidd == -1)
-		{ /* if the fork call failed */
+		{
 			perror(nick->c);
 			exit(EXIT_FAILURE);
 		}
 		if (pidd == 0)
-		{/* I am the child process, I execute the program*/
+		{
+			/* execute the program*/
 			retval = execve(nick->f[0], nick->f, nick->env);
-			if (retval == -1) /* if error when execve*/
+			if (retval == -1)
 				perror(nick->c), exit(EXIT_FAILURE);
 		}
 		else
-		{/* I am the father, I wait and check the exit status of the child */
+		{
+			/* wait, then exit status of the child */
 			wait(&status);
 			if (WIFEXITED(status))
 				errno = WEXITSTATUS(status);
