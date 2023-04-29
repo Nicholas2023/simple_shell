9ee7e2ac3a4b22cd1f1fc6f3b9a1bac1b0a635d1
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-04-29 05:39:08 +0300

    update

--- p_env.c
+++ p_env.c
@@ -1,88 +1,85 @@
 #include "alx.h"
 
+
 /**
- * env_get_key - gets the value of an environment variable
- * @key: the environment variable of interest
- * @nick: struct of the program's data
- * Return: a pointer to the value of the variable or NULL if it doesn't exist
+ * env_get_key - A function that gets env variables
+ * @key: A pointer to target env variable
+ * @nick: A pointer to struct
+ *
+ * Return: a pointer to the value of env variable
  */
+
+
 char *env_get_key(char *key, _st *nick)
 {
 	int i, key_length = 0;
 
-	/* validate the arguments */
 	if (key == NULL || nick->env == NULL)
 		return (NULL);
-
-	/* obtains the leng of the variable requested */
 	key_length = str_length(key);
-
 	for (i = 0; nick->env[i]; i++)
-	{/* Iterates through the environ and check for coincidence of the vame */
+	{
+		/*Transverses through the environ*/
 		if (str_compare(key, nick->env[i], key_length) &&
 		 nick->env[i][key_length] == '=')
-		{/* returns the value of the key NAME=  when find it*/
+		{
 			return (nick->env[i] + key_length + 1);
 		}
 	}
-	/* returns NULL if did not find it */
 	return (NULL);
 }
 
+
 /**
- * env_set_key - overwrite the value of the environment variable
- * or create it if does not exist.
- * @key: name of the variable to set
+ * env_set_key - A func that  overwrites env var
+ * @key: A pointer to env var
  * @value: new value
- * @nick: struct of the program's data
- * Return: 1 if the parameters are NULL, 2 if there is an erroror 0 if sucess.
+ * @nick: A pointer to struct
+ *
+ * Return: 1 if the parameters are NULL
  */
+
 
 int env_set_key(char *key, char *value, _st *nick)
 {
 	int i, key_length = 0, is_new_key = 1;
 
-	/* validate the arguments */
 	if (key == NULL || value == NULL || nick->env == NULL)
 		return (1);
-
-	/* obtains the leng of the variable requested */
 	key_length = str_length(key);
-
 	for (i = 0; nick->env[i]; i++)
-	{/* Iterates through the environ and check for coincidence of the vame */
+	{
 		if (str_compare(key, nick->env[i], key_length) &&
 		 nick->env[i][key_length] == '=')
-		{/* If key already exists */
+		{
 			is_new_key = 0;
-			/* free the entire variable, it is new created below */
 			free(nick->env[i]);
 			break;
 		}
 	}
-	/* make an string of the form key=value */
 	nick->env[i] = str_concat(str_duplicate(key), "=");
 	nick->env[i] = str_concat(nick->env[i], value);
 
 	if (is_new_key)
-	{/* if the variable is new, it is create at end of actual list and we need*/
-	/* to put the NULL value in the next position */
+	{
 		nick->env[i + 1] = NULL;
 	}
 	return (0);
 }
 
+
 /**
- * env_remove_key - remove a key from the environment
- * @key: the key to remove
- * @nick: the sructure of the program's data
- * Return: 1 if the key was removed, 0 if the key does not exist;
+ * env_remove_key - A func that remove a key from the env
+ * @key: A pointer to the key to remove
+ * @nick: A pointer to struct
+ * Return: 1 if the key was removed
  */
+
+
 int env_remove_key(char *key, _st *nick)
 {
 	int i, key_length = 0;
 
-	/* validate the arguments */
 	if (key == NULL || nick->env == NULL)
 		return (0);
 
@@ -90,10 +87,11 @@ int env_remove_key(char *key, _st *nick)
 	key_length = str_length(key);
 
 	for (i = 0; nick->env[i]; i++)
-	{/* iterates through the environ and checks for coincidences */
+	{
 		if (str_compare(key, nick->env[i], key_length) &&
 		 nick->env[i][key_length] == '=')
-		{/* if key already exists, remove them */
+		{
+			/* if key already exists, remove them */
 			free(nick->env[i]);
 
 			/* move the others keys one position down */
@@ -102,7 +100,6 @@ int env_remove_key(char *key, _st *nick)
 			{
 				nick->env[i - 1] = nick->env[i];
 			}
-			/* put the NULL value at the new end of the list */
 			nick->env[i - 1] = NULL;
 			return (1);
 		}
@@ -112,10 +109,13 @@ int env_remove_key(char *key, _st *nick)
 
 
 /**
- * print_environ - prints the current environ
- * @nick: struct for the program's data
- * Return: nothing
+ * print_environ - prints the current env variables
+ * @nick: A pointer to struct
+ *
+ * Return: void
  */
+
+
 void print_environ(_st *nick)
 {
 	int j;
