4bab328ae6f81165576309f960c9a65f4254cac7
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-04-29 03:15:13 +0300

    Update

--- func_library2.c
+++ func_library2.c
@@ -23,47 +23,47 @@ int _printe(char *string)
 
 /**
  * _print_error - writes a array of chars in the standart error
- * @data: a pointer to the program's data'
+ * @nick: a pointer to the program's data'
  * @errorcode: error code to print
  * Return: the number of bytes writed or .
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _print_error(int errorcode, data_of_program *data)
+int _print_error(int errorcode, _st *nick)
 {
 	char n_as_string[10] = {'\0'};
 
-	long_to_string((long) data->exec_counter, n_as_string, 10);
+	long_to_string((long) nick->d, n_as_string, 10);
 
 	if (errorcode == 2 || errorcode == 3)
 	{
-		_printe(data->program_name);
+		_printe(nick->a);
 		_printe(": ");
 		_printe(n_as_string);
 		_printe(": ");
-		_printe(data->tokens[0]);
+		_printe(nick->f[0]);
 		if (errorcode == 2)
 			_printe(": Illegal number: ");
 		else
 			_printe(": can't cd to ");
-		_printe(data->tokens[1]);
+		_printe(nick->f[1]);
 		_printe("\n");
 	}
 	else if (errorcode == 127)
 	{
-		_printe(data->program_name);
+		_printe(nick->a);
 		_printe(": ");
 		_printe(n_as_string);
 		_printe(": ");
-		_printe(data->command_name);
+		_printe(nick->c);
 		_printe(": not found\n");
 	}
 	else if (errorcode == 126)
 	{
-		_printe(data->program_name);
+		_printe(nick->a);
 		_printe(": ");
 		_printe(n_as_string);
 		_printe(": ");
-		_printe(data->command_name);
+		_printe(nick->c);
 		_printe(": Permission denied\n");
 	}
 	return (0);
